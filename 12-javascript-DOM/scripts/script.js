bb81965734b1f5c079f7295985d7b057d15a087b
Remi Thorez <remi.thorez@gmail.com>
2023-02-14 12:23:34 -0500

    Copy exercice 12

---
+++ 12-javascript-DOM/scripts/script.js
@@ -0,0 +1,31 @@
+// 1. Dessinez le modèle par arborescence du DOM (Document Object Model) de index.html
+
+
+// Éxecuter le Javascript après la création du DOM
+window.onload = () => {
+  // 2. Remplacez le texte du titre par "DOMaine des Arbres"
+  
+  
+  
+  // 3. Changez l'ordre de présentation des images avec flex
+
+
+
+  // 4. Ajoutez le point "Terre" à la liste d'éléments
+
+  
+
+  // 5. Ajoutez l'attribut href manquant à la balise lien de l'image d'arbre.
+  // avec la valeur  href="https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg"
+  // Avec le sélecteur suivant, utilisez un "accès par relation" pour faire votre modification.
+  const imageSansLien = document.querySelectorAll(
+    'img[src="https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg"]'
+  )[0];
+  // code ici
+
+
+  // 6. Modifiez la police du texte pour toutes les éléments de la liste
+
+
+
+};
