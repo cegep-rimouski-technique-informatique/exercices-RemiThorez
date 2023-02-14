b8486ddbad5c9905886bdb17fb81713b9738f5aa
Remi Thorez <remi.thorez@gmail.com>
2023-02-14 15:09:37 -0500

    Remise exercice 12

--- 12-javascript-DOM/scripts/script.js
+++ 12-javascript-DOM/scripts/script.js
@@ -4,16 +4,26 @@
 // Éxecuter le Javascript après la création du DOM
 window.onload = () => {
   // 2. Remplacez le texte du titre par "DOMaine des Arbres"
-  
-  
+document.getElementById("titre").outerHTML = "Domain des Arbres";
   
   // 3. Changez l'ordre de présentation des images avec flex
+  function changerDirFlex()
+  {
+    var element = document.getElementsByClassName("container");
+        var element2 = element[0]["style"]["flex-direction"] = "column-reverse";
 
+    console.log("HAHAHAHHAHAHAHA. . . :(                Send help . . .")
+  }
+  changerDirFlex();
 
 
   // 4. Ajoutez le point "Terre" à la liste d'éléments
+function ajouterElemento()
+{
+    var elementUl = document.getElementsByTagName("ul")[0].appendChild(document.createTextNode("Terre"));
+}
 
-  
+ajouterElemento();
 
   // 5. Ajoutez l'attribut href manquant à la balise lien de l'image d'arbre.
   // avec la valeur  href="https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg"
@@ -22,10 +32,29 @@ window.onload = () => {
     'img[src="https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg"]'
   )[0];
   // code ici
-
+  
+  function mettreImgLien()
+  {
+    var imagesSansLiens = document.querySelectorAll("img");
+    for(const image of imagesSansLiens) 
+    {
+      if(!image.parentNode.hasAttribute("href"))
+      {
+        image.parentNode.setAttribute("href", "https://www.gardeningknowhow.com/wp-content/uploads/2017/07/hardwood-tree.jpg")
+      }
+    }
+  }
+  mettreImgLien();
+ 
 
   // 6. Modifiez la police du texte pour toutes les éléments de la liste
 
 
+  function changementDePolice()
+  {
+    document.getElementsByTagName("ul")[0]["style"]["font-family"] = "arial";
+  }
 
-};
+  changementDePolice();
+
+}
