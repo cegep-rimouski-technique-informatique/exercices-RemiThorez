5e1b809f69f48d8a01aa33c328a0d60b82869ced
Remi Thorez <remi.thorez@gmail.com>
2023-02-23 13:53:23 -0500

    Work IN PROGRESS

--- 14-animations-asynchrones/main.js
+++ 14-animations-asynchrones/main.js
@@ -13,7 +13,45 @@ const spade1 = document.querySelector("#spade1");
 const spade2 = document.querySelector("#spade2");
 const spade3 = document.querySelector("#spade3");
 
-spade1.animate(spadeTumbling, spadeTiming);
 
-// codez ici
+// codez ici  
+/*function animationPique(nomItem, anime)
+{
+  spade1.animate(spadeTumbling, spadeTiming);
+}
+
+
+function animationPique2()
+{
+  spade2.animate(spadeTumbling, spadeTiming);
+}
+
+animationPique("N/A",animationPique2);
+*/
+
+
+const lesAnimations = [spade1, spade2, spade3];
+
+function prochain(theSpade)
+{
+
+  let i = lesAnimations.indexOf(theSpade)
+  if(i >= lesAnimations.length - 1)
+  { 
+    i = 0;
+  }
+  else
+  {
+    i++;
+  }
+  animer(lesAnimations[i], prochain);
+}
+
+animer(lesAnimations[0], prochain);
+
+function animer(item, callback) 
+{
+  item.animate(spadeTumbling, spadeTiming);
+  callback(item).delay(2000);
+}
 
